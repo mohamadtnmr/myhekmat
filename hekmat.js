2734fec82948beec9f3b59bf8b3c33beb70ea29f
mohamadtnmr <mohamad.tnmr@gmail.com>
2024-06-30 15:23:00 +0200

    Update hekmat.js

--- hekmat.js
+++ hekmat.js
@@ -115,14 +115,18 @@ function scrollToBottom() {
   }, 100);
 }
 
-function adjustViewportHeight() {
-  // First we get the viewport height and multiple it by 1% to get a value for a vh unit
+function setVHVariable() {
   let vh = window.innerHeight * 0.01;
-  // Then we set the value in the --vh custom property to the root of the document
   document.documentElement.style.setProperty("--vh", `${vh}px`);
 }
-window.addEventListener("resize", adjustViewportHeight);
-adjustViewportHeight();
+
+// Call the function on load
+setVHVariable();
+
+// Call the function on resize
+window.addEventListener("resize", () => {
+  setVHVariable();
+});
 
 function displayChatHistory(chatId) {
   const chat = chats[chatId];
